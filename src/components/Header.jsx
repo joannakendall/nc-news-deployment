4aeb84cdc544ede8cf448230e3a7cc583c8f5a24
joannakendall <58439846+joannakendall@users.noreply.github.com>
2020-05-11 10:16:48 +0100

    Styling and tidied up some code into seperate components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-//import styled from '../css/Header.module.css'
+import styled from '../css/Header.module.css'
 
 const Header = ({user}) => {
     return (
         <header className='header'>
-            <h1>NORTHCODERS NEWS!</h1>
-            <h2>You are logged in as {user}</h2>
+            <h1 className={styled.h1}>NORTHCODERS NEWS!</h1>
+            <h2 className={styled.h2}>You are logged in as {user}</h2>
         </header>
     );
 };
